bc1e9c003bdeb0f75dc8fc274a7674f9f559fe9e
nik <nik@nik>
2024-04-25 21:41:28 +0300

    дз1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Category.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Category.java
@@ -1,14 +1,12 @@
 package guru.qa.niffler.jupiter.annotation;
 
-import guru.qa.niffler.model.CurrencyValues;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 public @interface Category {
     String category();
 
