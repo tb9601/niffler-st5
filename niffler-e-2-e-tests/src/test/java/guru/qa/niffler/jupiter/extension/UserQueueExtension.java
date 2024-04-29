cd20758e45e02050b8fcd6ed7e990b5d2974cf37
nik <nik@nik>
2024-04-30 02:29:42 +0300

    fix

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UserQueueExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UserQueueExtension.java
@@ -1,18 +1,14 @@
 package guru.qa.niffler.jupiter.extension;
 
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.pages.RegistrationPage;
 import guru.qa.niffler.pages.WelcomePage;
 import guru.qa.niffler.utils.UserUtils;
-import io.qameta.allure.Allure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.*;
-import org.junit.platform.commons.support.AnnotationSupport;
 
-import javax.xml.stream.events.Namespace;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.*;
@@ -21,7 +17,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 
 import static guru.qa.niffler.jupiter.annotation.User.Selector.*;
-import static guru.qa.niffler.model.UserJson.simpleUser;
 
 
 public class UserQueueExtension implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback, ParameterResolver {
@@ -156,121 +151,3 @@ public class UserQueueExtension implements BeforeAllCallback, BeforeEachCallback
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-// Любой тест проходит через него
-/*public class UserQueueExtension implements
-        BeforeAllCallback,
-        BeforeEachCallback,
-        AfterEachCallback,
-        ParameterResolver {
-
-    public static final ExtensionContext.Namespace NAMESPACE
-            = ExtensionContext.Namespace.create(UserQueueExtension.class);
-
-    private static final Queue<UserJson> USERS_WITHOUT_INVITATIONS_AND_FRIENDS = new ConcurrentLinkedQueue<>();
-    private static final Queue<UserJson> INVITATION_SEND = new ConcurrentLinkedQueue<>();
-    private static final Queue<UserJson> INVITATION_RECIEVED = new ConcurrentLinkedQueue<>();
-    private static final Queue<UserJson> WITH_FRIENDS = new ConcurrentLinkedQueue<>();
-
-
-    @Override
-    public void beforeAll(ExtensionContext extensionContext) throws Exception {
-        final WelcomePage welcomePage = new WelcomePage();
-        final RegistrationPage registrationPage = new RegistrationPage();
-
-        UserJson tempUser;
-        for (int i = 0; i < 2; i++) {
-            tempUser = UserUtils.generateRandomUserJson();
-            Selenide.open("http://127.0.0.1:3000/main");
-            welcomePage.registerBtnClick();
-            registrationPage.fillRegistrationPage(tempUser)
-                    .clickSignInButton();
-            USERS_WITHOUT_INVITATIONS_AND_FRIENDS.add(tempUser);
-        }
-        Selenide.closeWebDriver();
-    }
-
-    @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
-        UserJson userForTest = null;
-        ExtensionContext.Store store = context.getStore(NAMESPACE);
-        List<User> users = Arrays.stream(context.getRequiredTestMethod().getParameters())
-                .filter(p -> AnnotationSupport.isAnnotated(p, User.class))
-                .map(p -> p.getAnnotation(User.class)).collect(Collectors.toList());
-            if (users.isEmpty()) { //если аннотациии в тесте нету
-                while (userForTest == null) {
-                    userForTest = USERS_WITHOUT_INVITATIONS_AND_FRIENDS.poll();
-                }
-                ArrayList<UserJson> tempQueue = new ArrayList<>();
-                tempQueue.add(userForTest);
-                store.put(context.getUniqueId() + "_" + User.Selector.WITHOUT_INVITATIONS_AND_FRIENDS, tempQueue);
-            } else {
-                for (int i = 0; i < users.size(); i++) {
-                    while (userForTest == null) {
-                        if (users.get(i).selector().equals(User.Selector.WITHOUT_INVITATIONS_AND_FRIENDS)) {
-                            userForTest = USERS_WITHOUT_INVITATIONS_AND_FRIENDS.poll();
-                        } else if (users.get(i).selector().equals(User.Selector.INVITATION_SEND)) {
-                            userForTest = INVITATION_SEND.poll();
-                        } else if (users.get(i).selector().equals(User.Selector.INVITATION_RECEIVED)) {
-                            userForTest = INVITATION_RECIEVED.poll();
-                        } else if (users.get(i).selector().equals(User.Selector.WITH_FRIENDS)) {
-                            userForTest = WITH_FRIENDS.poll();
-                        }
-                    }
-                    ArrayList<UserJson> tempQueue;
-                    if (store.get(context.getUniqueId() + "_" + users.get(i).selector().name(), ArrayList.class) == null) {
-                        tempQueue = new ArrayList<>();
-                        tempQueue.add(userForTest);
-                    } else {
-                        tempQueue = store.get(context.getUniqueId() + "_" + users.get(i).selector().name(), ArrayList.class);
-                        tempQueue.add(userForTest);
-                    }
-                    store.put(context.getUniqueId() + "_" + users.get(i).selector().name(), tempQueue);
-                    userForTest = null;
-                }
-            }
-        Allure.getLifecycle().updateTestCase(testCase -> {
-            testCase.setStart(new Date().getTime());
-        });
-    }
-
-    @Override
-    public void afterEach(ExtensionContext context) throws Exception {
-        context.getTestMethod().get().getParameters();
-        ExtensionContext.Store store = context.getStore(NAMESPACE);
-        ArrayList tempList = store.get(context.getUniqueId() + "_" + User.Selector.WITHOUT_INVITATIONS_AND_FRIENDS, ArrayList.class);
-        UserJson user = (UserJson) tempList.getLast();
-        tempList.remove(user);
-        store.put(context.getUniqueId() + "_" + User.Selector.WITHOUT_INVITATIONS_AND_FRIENDS, tempList);
-        USERS_WITHOUT_INVITATIONS_AND_FRIENDS.add(user);
-    }
-
-
-    @Override
-    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return parameterContext
-                .getParameter()
-                .getType()
-                .isAssignableFrom(UserJson.class);
-    }
-
-    @Override
-    public UserJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        ExtensionContext.Store store = extensionContext.getStore(NAMESPACE);
-        if (parameterContext.getParameter().getAnnotation(User.class) != null) {
-            User.Selector annotation = parameterContext.getParameter().getAnnotation(User.class).selector();
-            return (UserJson) store.get(extensionContext.getUniqueId() + "_" + annotation, ArrayList.class).getLast();
-        } else {
-            return (UserJson) store.get(extensionContext.getUniqueId() + "_" + User.Selector.WITHOUT_INVITATIONS_AND_FRIENDS, ArrayList.class).getLast();
-        }
-    }
-}*/
